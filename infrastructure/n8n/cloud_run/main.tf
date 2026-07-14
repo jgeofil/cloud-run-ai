389f70811a0cfd2979b1ab3e92aa7197d588c7c3
jgeofil <jeremy.geo@gmail.com>
2026-07-14 17:06:08 +0000

    Fix env block syntax in n8n Cloud Run Terraform config

--- infrastructure/n8n/cloud_run/main.tf
+++ infrastructure/n8n/cloud_run/main.tf
@@ -43,11 +43,13 @@ resource "google_cloud_run_v2_service" "service" {
     service_account = var.service_account_email != "" ? var.service_account_email : google_service_account.workload.email
     containers {
       image = var.image
-      env = [
-        for key, value in var.environment_variables : {
-          name  = key
-          value = value
+      dynamic "env" {
+        for_each = var.environment_variables
+        content {
+          name  = env.key
+          value = env.value
         }
+      }
       ]
       ports {
         name        = "http1"
